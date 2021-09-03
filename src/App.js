b0515b5c6a06bad6d17b93e3e69a2d0410aa1816
YouJeen-ru <1.rouch@mail.ru>
2021-09-03 19:31:55 +0300

    add new state card btn, favorite

--- src/App.js
+++ src/App.js
@@ -29,23 +29,28 @@ function App() {
                         <input type="text" placeholder='Поиск...'/>
                     </div>
                 </div>
-
-                <div className="card">
-                    <img width={133} height={112} src="/img/sneakers/1.jpg" alt=""/>
-                    <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-                    <div className='d-flex justify-between align-center'>
-                        <div className='d-flex flex-column'>
-                            <span>Цена:</span>
-                            <b>12 999 руб.</b>
+                <div>
+                    <div className="card">
+                        <div className='favorite'>
+                            <img src="/img/heart-liked.svg" alt="Unliked"/>
                         </div>
-                        <button className='button'>
-                            <img width={11} height={11} src="/img/plus.svg" alt=""/>
-                        </button>
+                            <img width={133} height={112} src="/img/sneakers/1.jpg" alt=""/>
+                            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+                            <div className='d-flex justify-between align-center'>
+                                <div className='d-flex flex-column'>
+                                    <span>Цена:</span>
+                                    <b>12 999 руб.</b>
+                                </div>
+                                <button className='button'>
+                                    <img width={11} height={11} src="/img/plus.svg" alt=""/>
+                                </button>
+                            </div>
                     </div>
                 </div>
+
             </div>
         </div>
-    );
+);
 }
 
 export default App;
