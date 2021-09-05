b653076999ada64a76499e05c623fd2931c0b54b
YouJeen-ru <1.rouch@mail.ru>
2021-09-05 10:16:51 +0300

    add new components Card,Drawer,Header

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+  return (
+      <header className='d-flex justify-between p-40'>
+          <div className='d-flex align-center'>
+              <img width={40} height={40} src="/img/logo.png" alt="log"/>
+              <div>
+                  <h3 className='text-uppercase'>React Sneakers</h3>
+                  <p className='opacity-5'>Магазин лучших кроссовок</p>
+              </div>
+          </div>
+          <ul className='d-flex'>
+              <li className='mr-30'>
+                  <img width={18} height={18} src="/img/cart.svg" alt="log"/>
+                  <span>1205 руб.</span>
+              </li>
+              <li>
+                  <img width={18} height={18} src="/img/user.svg" alt="log"/>
+              </li>
+          </ul>
+      </header>
+  )
+}
+
+export default Header
