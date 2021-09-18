e2bcb82a82386ab0bd663cc561019eaaf890def1
YouJeen-ru <1.rouch@mail.ru>
2021-09-18 16:38:31 +0300

    add on project react-router-dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
 const Header = ({onClickCart}) => {
   return (
       <header className='d-flex justify-between p-40'>
+          <Link to='/'>
           <div className='d-flex align-center'>
               <img width={40} height={40} src="/img/logo.png" alt="log"/>
               <div>
@@ -10,13 +12,16 @@ const Header = ({onClickCart}) => {
                   <p className='opacity-5'>Магазин лучших кроссовок</p>
               </div>
           </div>
+          </Link>
           <ul className='d-flex'>
               <li onClick={onClickCart} className='mr-30 cu-p'>
                   <img width={18} height={18} src="/img/cart.svg" alt="log"/>
                   <span>1205 руб.</span>
               </li>
               <li className='mr-20 cu-p'>
-                  <img width={18} height={18} src="/img/heart.svg" alt="log"/>
+                  <Link to='/favorites'>
+                      <img width={18} height={18} src="/img/heart.svg" alt="log"/>
+                  </Link>
               </li>
               <li>
                   <img width={18} height={18} src="/img/user.svg" alt="log"/>
