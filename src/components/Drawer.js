8b37e6e20a2d8704965eb64535a725e9c3a5b81a
YouJeen-ru <1.rouch@mail.ru>
2021-09-21 19:41:58 +0300

    add Info , min refactoring

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, {useContext, useState} from "react";
+import Info from "./Info";
+import {AppContext} from "../context";
 
 const Drawer = ({onClose, onRemove, cartItems = []}) => {
+    const { setCartItems } = useContext(AppContext)
+
+    const [isComplete, setIsComplete] = useState(false)
+
+    const onClickOrder = () => {
+      setIsComplete(true)
+        setCartItems([])
+    }
     return (
         <div className="overlay">
             <div className="drawer justify-between">
@@ -42,21 +52,20 @@ const Drawer = ({onClose, onRemove, cartItems = []}) => {
                                     </li>
                                 </ul>
 
-                                <button className='greenButton'>
+                                <button onClick={onClickOrder} className='greenButton'>
                                     Оформить заказ <img src="/img/arrow.svg" alt="arrow"/>
                                 </button>
                             </div>
                         </div>
                         :
-                        <div className="cartEmpty d-flex align-center justify-center flex-column flex">
-                            <img className="mb-20" width="120px" height="120px" src="/img/empty-cart.jpg" alt="Empty"/>
-                            <h2>Корзина пустая</h2>
-                            <p className="opacity-6">Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
-                            <button onClick={onClose} className="greenButton">
-                                <img src="/img/arrow.svg" alt="Arrow"/>
-                                Вернуться назад
-                            </button>
-                        </div>
+                        <Info
+                            title={isComplete ? 'Заказ оформлен!' :'Корзина пустая'}
+                            description={isComplete
+                                ? 'Ваш заказ #18 скоро будет передан курьерской доставке'
+                                : 'Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.'}
+                            image={isComplete ? '/img/complete-order.jpg' : '/img/empty-cart.jpg'}
+                        />
+
                 }
 
 
